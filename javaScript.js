ff0ea1dde6f2e172bf85419f8e9aa46888815679
Rafael Diaz <diazmontano97@gmail.com>
2024-02-16 22:51:37 -0400

    aca vamos

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+const j= document.querySelector(".j");
+const u= document.querySelector(".u");
+const a= document.querySelector(".a");
+const n= document.querySelector(".n");
+
+
+
+
+j.addEventListener("click", function(){
+    alert("No pinches aqui, que crece mi amor por ti <3");
+})
+u.addEventListener("click", function(){
+    alert("Otra vez, que pena");
+})
+a.addEventListener("click", function(){
+    alert("<3");
+})
+n.addEventListener("click", function(){
+    alert("Oh no, morire de amor");
+})
