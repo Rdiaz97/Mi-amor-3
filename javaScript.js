cd57b70205f774f6e7d7875549a631f1366bed5f
Rafael Diaz <diazmontano97@gmail.com>
2024-02-17 02:11:04 -0400

    agregamos js del bueno

--- javaScript.js
+++ javaScript.js
@@ -2,8 +2,9 @@ const j= document.querySelector(".j");
 const u= document.querySelector(".u");
 const a= document.querySelector(".a");
 const n= document.querySelector(".n");
-
-
+const img1= document.querySelector(".img1");
+const img2= document.querySelector(".img2");
+const img3= document.querySelector(".img3");
 
 
 j.addEventListener("click", function(){
@@ -17,4 +18,24 @@ a.addEventListener("click", function(){
 })
 n.addEventListener("click", function(){
     alert("Oh no, morire de amor");
+})
+
+j.addEventListener("click", function(){
+    j.classList.add("animation-rigth");
+})
+
+u.addEventListener("click", function(){
+    u.classList.add("animation-left");
+})
+
+img1.addEventListener("click", function(){
+    j.classList.remove("animation-rigth");
+})
+
+img2.addEventListener("click", function(){
+    j.classList.remove("animation-rigth");
+})
+
+img3.addEventListener("click", function(){
+    u.classList.remove("animation-left");
 })
